d746ed09ed3b679056b2446726c4039ad2c7ccf2
robelMike <robelmichael102@gmail.com>
2020-02-17 19:44:15 +0100

    new change

--- app.py
+++ app.py
@@ -12,13 +12,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 class fixdb(db.Model):
-	__tablename__ = 'dht'
+	__tablename__ = 'dht_new'
 
 	id = db.Column(db.Integer, primary_key =True)
 	name = db.Column(db.String(80))
@@ -28,7 +23,6 @@ class fixdb(db.Model):
 		self.temp = temp
 
 	def add_to_db(self):
-		db.create_all()
 		db.session.add(self)
 		db.session.commit()
 
@@ -36,15 +30,19 @@ class fixdb(db.Model):
 @app.route('/get', methods=['GET'])
 def postrandom():
 	temp = {'temperature': '23'}
+	db.create_all() 
 	test_temp = fixdb(temp)
-
 	test_temp.add_to_db()
+	return jsonify(temp)
 
+@app.route('/list', methods=['GET'])
+def postrandom():
+	temp = {'temperature': '23'}
+	db.create_all() 
+	test_temp = fixdb(temp)
+	test_temp.add_to_db()
 	return jsonify(temp)
 
 
-
 if __name__ == '__main__':
-	from db import db
-	db.init_app(app)
 	app.run(port=5000, debug=True)
--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 from app import app
-from db import db
+from app import create_tables
 
 db.init_app(app)
-
 @app.before_first_request
 def create_tables():
 	db.create_all()
